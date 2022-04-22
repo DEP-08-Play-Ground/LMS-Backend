c72a8dcd3c6a99274e91beed30811e80905ead7a
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-04-22 11:27:44 +0530

    Add JSONB to the project

---
+++ src/main/java/dep8/ijse/lk/lmsbackend/Customer.java
@@ -0,0 +1,53 @@
+package dep8.ijse.lk.lmsbackend;
+
+import jakarta.json.bind.annotation.JsonbProperty;
+
+public class Customer {
+    private String id;
+
+    @JsonbProperty("full_name")
+    private String name;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- src/main/java/dep8/ijse/lk/lmsbackend/StudentServlet.java
+++ src/main/java/dep8/ijse/lk/lmsbackend/StudentServlet.java
@@ -1,9 +1,16 @@
 package dep8.ijse.lk.lmsbackend;
 
+import jakarta.json.*;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StudentServlet extends HttpServlet {
@@ -19,11 +26,75 @@ public class StudentServlet extends HttpServlet {
             return;
         }
 
-        BufferedReader reader = request.getReader();
-        StringBuilder stb = new StringBuilder();
-        Stream<String> lines = reader.lines();
-        lines.forEach(s -> stb.append(s+"\n"));
-        System.out.println(stb.toString());
+//        BufferedReader reader = request.getReader();
+//        StringBuilder stb = new StringBuilder();
+//        Stream<String> lines = reader.lines();
+//        lines.forEach(s -> stb.append(s+"\n"));
+//        System.out.println(stb.toString());
+
+        /*=====================================================================*/
+
+//        JsonReader jsonReader = Json.createReader(request.getReader());
+//        JsonArray jsonArray = jsonReader.readArray();
+//        for (int i = 0; i < jsonArray.size(); i++) {
+//            JsonObject jsonObject = jsonArray.getJsonObject(0);
+//            System.out.println(jsonObject.getString("id"));
+//            System.out.println(jsonObject.getString("name"));
+//        }
+
+        /*=====================================================================*/
+
+
+//        response.setContentType("application/json");
+//        JsonWriter jsonWriter = Json.createWriter(response.getWriter());
+//        JsonObjectBuilder obj = Json.createObjectBuilder();
+//        obj.add("id","C005");
+//        obj.add("name","Ruwan");
+//        JsonObject build = obj.build();
+//        jsonWriter.writeObject(build);
+
+        /*=====================================================================*/
+
+//        response.setContentType("application/json");
+//        JsonWriter jsonWriter = Json.createWriter(response.getWriter());
+//        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+//        arrayBuilder.add(0,10);
+//        arrayBuilder.add(1,false);
+//        arrayBuilder.add(2,Json.createArrayBuilder().add(10).add(20).build());
+//
+//        /*=====================================================================*/
+//
+//        //language=JSON
+////        String json= "{\n" +
+////                "  \"id\":" +
+////                " \"C001\",\n" +
+////                "  \"name\": \"Kasun\"\n" +
+////                "}\n";
+//
+//        /*=====================================================================*/
+//        arrayBuilder.add(3,Json.createObjectBuilder().add("C001","Kasun").build());
+//        jsonWriter.writeArray(arrayBuilder.build());
+
+        Jsonb jsonb = JsonbBuilder.create();
+        List<Customer> customerList = jsonb.fromJson(request.getReader(),
+                new ArrayList<Customer>(){}.getClass().getGenericSuperclass());
+
+        customerList.forEach(System.out::println);
+
+        /*=====================================================================*/
+
+        List<Customer> anotherCustomerList = new ArrayList<>();
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+
+        response.setContentType("application/json");
+        jsonb.toJson(anotherCustomerList, response.getWriter());
+
+        /*=============================================================*/
+
     }
 
     @Override
